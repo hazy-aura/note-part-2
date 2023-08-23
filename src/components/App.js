a78f6b8aa46dc20d0ae96d1c74dee3e1553fc008
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2023-08-23 18:15:47 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+function NoteOne(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(NoteOne)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
